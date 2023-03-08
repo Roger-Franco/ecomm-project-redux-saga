6390e7e7ff5bb9d2bbcc098ec0a0c74eba2621c9
Roger Franco <rogerfrancogalt@gmail.com>
2023-03-07 21:58:35 -0400

    Reducer | How to make reducer in Redux

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,7 @@
 import {createStore} from 'redux'
+import rootReducer from './rootReducer'
 
-const dummyReducer = () => { // so ora funcionar momentaneamente
-  return 100
-}
-const store = createStore(dummyReducer)
+
+const store = createStore(rootReducer)
 
 export default store;
