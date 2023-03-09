877d9ca1db6c0bc13d9764f68f7507413dd72e91
Roger Franco <rogerfrancogalt@gmail.com>
2023-03-09 10:57:59 -0400

    validação no "length" para nao dar erro

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -7,7 +7,7 @@ export const cartData = (data=[], action) => {
       return [action.data, ...data]
     case REMOVE_FROM_CART:
   console.log("REMOVE_FROM_CART condition", action );
-      data.length = data.length -1
+  data.length = data.length ? data.length -1 : [] // validação no "length" para nao dar erro
       return [...data]
     case EMPTY_CART:
   console.log("EMPTY_CART condition", action );
