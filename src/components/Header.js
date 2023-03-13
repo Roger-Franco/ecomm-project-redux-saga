66b18e85c728b04aab6fb2adef012c6d2cd80d6c
Roger Franco <rogerfrancogalt@gmail.com>
2023-03-13 12:06:25 -0400

    Display Search Cart Product

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,18 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 import { Link } from 'react-router-dom';
+import { productSearch } from '../redux/productAction';
 
 export const Header = () => {
   const result = useSelector((state) => state.cartData)
+  const dispatch = useDispatch()
   console.log("data in header", result);
   return (
     <div className='header'>
       <Link to="/"><h1 className='logo'>E-Comm</h1></Link>
+      <div className='search-box'>
+        <input onChange={(e) => dispatch(productSearch(e.target.value))} type="text" placeholder="Search Product" />
+      </div>
       <Link to="/cart">
     <div className="cart-div">
       <span>{result.length}</span>
--- src/redux/productSaga.js
+++ src/redux/productSaga.js
@@ -1,5 +1,5 @@
 import {takeEvery, put} from 'redux-saga/effects'
-import {PRODUCT_LIST, SET_PRODUCT_LIST} from './constant'
+import {PRODUCT_LIST, SET_PRODUCT_LIST, PRODUCT_SEARCH} from './constant'
 
 function* getProducts() {
   let data = yield fetch('http://localhost:3500/products')
@@ -8,8 +8,16 @@ function* getProducts() {
   yield put({type: SET_PRODUCT_LIST, data})
 }
 
+function* searchProducts(data) {
+  let result = yield fetch(`http://localhost:3500/products?q=${data.query}`)
+  result = yield result.json()
+  console.log("action is called", result);
+  yield put({type: SET_PRODUCT_LIST, data: result})
+}
+
 function* productSaga() {
   yield takeEvery(PRODUCT_LIST, getProducts)
+  yield takeEvery(PRODUCT_SEARCH, searchProducts)
 }
 
 export default productSaga
